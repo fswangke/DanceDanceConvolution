66654b65c21f56edd9c7436a1eaa64621b51aac6
Ke Wang <fswangke@gmail.com>
2017-04-18 14:22:17 -0400

    Tensorflow lib added. Camera preview set-up.

---
+++ app/src/main/java/unc/cs/v3d/dancedanceconvolution/MainActivity.java
@@ -0,0 +1,100 @@
+package unc.cs.v3d.dancedanceconvolution;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.YuvImage;
+import android.media.ImageReader;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.flurgle.camerakit.CameraListener;
+import com.flurgle.camerakit.CameraView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private CameraView mCameraPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        mCameraPreview = (CameraView)findViewById(R.id.camera_preview);
+        mCameraPreview.setCameraListener(new CameraListener() {
+            @Override
+            public void onCameraOpened() {
+                super.onCameraOpened();
+            }
+
+            @Override
+            public void onCameraClosed() {
+                super.onCameraClosed();
+            }
+
+            @Override
+            public void onPictureTaken(byte[] jpeg) {
+                super.onPictureTaken(jpeg);
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length);
+                bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, false);
+
+                // TODO: fire-up tensorflow inference
+            }
+
+            @Override
+            public void onPictureTaken(YuvImage yuv) {
+                super.onPictureTaken(yuv);
+            }
+
+            @Override
+            public void onVideoTaken(File video) {
+                super.onVideoTaken(video);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+
+        mCameraPreview.start();
+        // TODO: restart the inference thread here
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        mCameraPreview.stop();
+        // TODO: pause the inference thread here
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+}
