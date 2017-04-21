39aa867afea62b28fd094cb794b7cf2efe14d297
Ke Wang <fswangke@gmail.com>
2017-04-21 18:22:24 -0400

    fix fetch tensorflow result bug

--- app/src/main/java/unc/cs/v3d/dancedanceconvolution/PoseMachine.java
+++ app/src/main/java/unc/cs/v3d/dancedanceconvolution/PoseMachine.java
@@ -18,26 +18,40 @@ public class PoseMachine {
     private boolean mIfLogStats = false;
 
     // Network config
-    private String inputName;
-    private String outputName;
-    private int inputSize;
-    private int inputWidth;
-    private int inputHeight;
-    private int imageMean;
-    private int imageStd;
+    private String mInputName;
+    private int mInputWidth;
+    private int mInputHeight;
 
     // pre-allocated buffers
-    private int[] intValues;
-    private float[] floatValues;
-    private float[] outputs;
-    private String[] outputNames;
+    private int[] mInputIntBuffer;
+    private float[] mInputFloatBuffer;
+    private float[] mOutputPAF;
+    private float[] mOutputHeatmap;
+    private String[] mOutputNames;
+
+    int getTensorSize(final Operation op) {
+        final int outputTensorRank = op.output(0).shape().numDimensions();
+        int outputSize = 1;
+        String outputDim = "[";
+        for (int i = 0; i < outputTensorRank; ++i) {
+            outputSize *= op.output(0).shape().size(i);
+            outputDim += op.output(0).shape().size(i);
+            if (i < outputTensorRank - 1) {
+                outputDim += "*";
+            }
+        }
+        outputDim += "]";
+        Log.i(TAG, "Output layer " + op.name() + " size:" + outputDim);
+
+        return outputSize;
+    }
 
     // Singleton pattern
     public static PoseMachine getPoseMachine(AssetManager assetManager, String modelFilename,
                                              int inputSize, String inputName, String[] outputNames) {
         PoseMachine pm = new PoseMachine();
-        pm.inputName = inputName;
-        pm.outputNames = outputNames;
+        pm.mInputName = inputName;
+        pm.mOutputNames = outputNames;
 
         try {
             pm.mTFinferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFilename);
@@ -45,22 +59,30 @@ public class PoseMachine {
             Log.e(TAG, "Exception:", e);
             throw e;
         }
-        int maxOutputSize = Integer.MIN_VALUE;
-        for (final String outputName : pm.outputNames) {
-            final Operation operation = pm.mTFinferenceInterface.graphOperation(outputName);
-            final int outputSize = (int) operation.output(0).shape().size(1);
-            Log.i(TAG, "Output layer " + outputName + " size:" + outputSize);
-            maxOutputSize = Math.max(maxOutputSize, outputSize);
-        }
-        Log.i(TAG, "Allocated buffer size for output:" + maxOutputSize);
+        final Operation heatmapOp = pm.mTFinferenceInterface.graphOperation(outputNames[1]);
+        final int heatmapOutputTensorSize = pm.getTensorSize(heatmapOp);
+        pm.mOutputHeatmap = new float[heatmapOutputTensorSize];
+        Log.i(TAG, "Allocated output buffer size for heatmap:" + heatmapOutputTensorSize);
 
-        pm.inputWidth = inputSize;
-        pm.inputHeight = inputSize;
-        pm.intValues = new int[pm.inputWidth * pm.inputHeight];
-        pm.floatValues = new float[pm.inputWidth * pm.inputHeight * 3];
-        pm.outputs = new float[maxOutputSize];
+        final Operation pafOp = pm.mTFinferenceInterface.graphOperation(outputNames[0]);
+        final int pafOutputTensorSize = pm.getTensorSize(pafOp);
+        pm.mOutputPAF = new float[pafOutputTensorSize];
+        Log.i(TAG, "Allocated output buffer size for paf:" + pafOutputTensorSize);
+
+        pm.mInputWidth = inputSize;
+        pm.mInputHeight = inputSize;
+        pm.mInputIntBuffer = new int[pm.mInputWidth * pm.mInputHeight];
+        pm.mInputFloatBuffer = new float[pm.mInputWidth * pm.mInputHeight * 3];
 
         return pm;
+    }
+
+    public float[] getHeatmap() {
+        return mOutputHeatmap;
+    }
+
+    public float[] getPAFs() {
+        return mOutputPAF;
     }
 
     public void inferPose(Bitmap bitmap) {
@@ -68,19 +90,20 @@ public class PoseMachine {
 
         Trace.beginSection("preprocessBitmap");
         // TODO: faster way of preprocessing images?
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-        for (int i = 0; i < intValues.length; ++i) {
-            final int val = intValues[i];
-            floatValues[i * 3 + 0] = (float) ((val >> 16) & 0xFF) / 255.0f - 0.5f;
-            floatValues[i * 3 + 1] = (float) ((val >> 8) & 0xFF) / 255.0f - 0.5f;
-            floatValues[i * 3 + 2] = (float) (val & 0xFF) / 255.0f - 0.5f;
+        bitmap.getPixels(mInputIntBuffer, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < mInputIntBuffer.length; ++i) {
+            final int val = mInputIntBuffer[i];
+            mInputFloatBuffer[i * 3 + 0] = (float) ((val >> 16) & 0xFF) / 255.0f - 0.5f;
+            mInputFloatBuffer[i * 3 + 1] = (float) ((val >> 8) & 0xFF) / 255.0f - 0.5f;
+            mInputFloatBuffer[i * 3 + 2] = (float) (val & 0xFF) / 255.0f - 0.5f;
         }
         Trace.endSection(); // preprocessBitmap
 
-        mTFinferenceInterface.feed(inputName, floatValues, 1, inputHeight, inputWidth, 3);
-        mTFinferenceInterface.run(outputNames, mIfLogStats);
+        mTFinferenceInterface.feed(mInputName, mInputFloatBuffer, 1, mInputHeight, mInputWidth, 3);
+        mTFinferenceInterface.run(mOutputNames, mIfLogStats);
         // TODO: fetch the result and perform parsing to get skelton
-//        mTFinferenceInterface.fetch(outputName, outputs);
+        mTFinferenceInterface.fetch(mOutputNames[0], mOutputPAF);
+        mTFinferenceInterface.fetch(mOutputNames[1], mOutputHeatmap);
 
         Trace.endSection(); // inferPose
     }
